4bd3fdd06570422d976a0a2746b3fc8394bfc9e2
evanschrager <evan.m.schrager@gmail.com>
2015-06-02 10:46:13 -0400

    edit post and team model, more associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   has_one :league, through: :event
+  has_one :sport, through: :league
+  has_one :team, through: :user
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,11 @@
 class Team < ActiveRecord::Base
+  attr_accessor :roster
+  
   belongs_to :league
   has_one :sport, through: :league
 
   has_many :users, through: :user_teams
   has_many :events, through: :league
   has_many :posts, through: :users
+
 end
