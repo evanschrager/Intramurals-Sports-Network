981856073986e11d85fabf320c3be28f343ee0d2
evros001 <evanrosse@gmail.com>
2015-06-05 09:45:18 -0400

    teams can add games and games can have teams

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,12 @@ class GamesController < ApplicationController
   end 
 
   def create
+    @game = Game.create(:game_info => params[:game][:game_info], :game_time => params[:game][:game_time], :location => params[:game][:location])
+    @team = Team.find(params[:game][:team_id].to_i)
+    @team.games << @game
     binding.pry
+  end 
+
+  def game_params
   end 
 end
